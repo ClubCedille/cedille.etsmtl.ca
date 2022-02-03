1eebc2e723b3bf64ce632a2d274ca1627f365b1a
Edvin Honda <edvin_honda@hotmail.com>
2022-02-03 14:31:23 -0500

    Test2

--- themes/cedille/static/js/main.js
+++ themes/cedille/static/js/main.js
@@ -1,32 +1,22 @@
-/*const slideMenu = () => {
-    const burger = document.getElementsByClassName(".burger");
-    const navLinks = document.getElementsByClassName(".navbar");
-
-    burger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-    })
-}
-
-slideMenu();*/
-
-
 const slideMenu = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".navbar ul");
-    const navLinks = document.querySelectorAll(".navbar ul li");
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navbar');
+    //const navLinks = document.querySelectorAll('.navbar ul li');
 
-    burger.addEventListener("click", () =>{
-        nav.classList.toggle(".navbar.active");
+    burger.addEventListener('click', () =>{
+        nav.classList.toggle('.navbar.active');
 
+        /*
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
-                link.style.animation = "";    
+                link.style.animation = '';    
             }else{
-                link.style.animation = "navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s"
+                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 5 + 1.5}s'
             }
         });
+        */
 
-        burger.classList.toggle("toggle");
+        burger.classList.toggle('toggle');
     });
 }
 
