2874820b85353d78bff65ab7d0854b87a24f8517
Edvin Honda <edvin_honda@hotmail.com>
2022-02-03 21:23:03 -0500

    css and js momdified

--- themes/cedille/static/js/main.js
+++ themes/cedille/static/js/main.js
@@ -1,21 +1,9 @@
 const slideMenu = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.navbar');
-    //const navLinks = document.querySelectorAll('.navbar ul li');
 
     burger.addEventListener('click', () =>{
         nav.classList.toggle('.navbar.active');
-
-        /*
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';    
-            }else{
-                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 5 + 1.5}s'
-            }
-        });
-        */
-
         burger.classList.toggle('toggle');
     });
 }
