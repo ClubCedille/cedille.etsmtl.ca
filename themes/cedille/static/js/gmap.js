60839db41beaef60500a374199b3d523df8307e9
Edvin Honda <edvin_honda@hotmail.com>
2023-08-31 21:34:48 -0400

    fix value on select form and gmap longitude and latitude

--- themes/cedille/static/js/gmap.js
+++ themes/cedille/static/js/gmap.js
@@ -1,5 +1,5 @@
 function initMap() {
-    var location = {lat: 45.493823, lng: -73.562332};
+    var location = {lat: 45.49348835090807, lng: -73.56258114157671};
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
         center: location
