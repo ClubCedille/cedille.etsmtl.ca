a46dbfb83196c2bdc7be5664a0b5c1fe7578f561
Edvin Honda <edvin_honda@hotmail.com>
2023-09-04 06:18:19 -0400

    fix latitude and longitude

--- themes/cedille/static/js/gmap.js
+++ themes/cedille/static/js/gmap.js
@@ -1,5 +1,5 @@
 function initMap() {
-    var location = {lat: 45.49348835090807, lng: -73.56258114157671};
+    var location = {lat: 45.4934676539119, lng: -73.56275806246913};
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
         center: location
