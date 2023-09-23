554031d665246bbd1e864cd4d48851a456cf2843
Edvin Honda <edvin_honda@hotmail.com>
2023-09-23 15:11:14 -0400

    adding notification bar

--- themes/cedille/static/js/main.js
+++ themes/cedille/static/js/main.js
@@ -1,24 +1,2 @@
-// //Javascript to enable link to tab
-// var hash = location.hash.replace(/^#/, '');  // ^ means starting, meaning only match the first hash
-// if (hash) {
-//     $('a[href="#' + hash + '"]').tab('show');
-// } 
 
-// // Change hash for page-reload
-// $('.nav-link').on('shown.bs.tab', function (e) {
-//     window.location.hash = e.target.hash;
-// })
-
-// function test() {
-//     var url = document.location.toString();
-// if (url.match('#')) {
-//     $('a[href="#' + url.split('#')[1] + '"]').tab('show');
-//     console.log(url);
-// }
-
-// // Change hash for page-reload
-// $('a').on('shown.bs.tab', function (e) {
-//     window.location.hash = e.target.hash;
-// })
-// }
 
